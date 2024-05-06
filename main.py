069333479c4161049c531f4bceb2eba82ed6e4cf
damuljumong <83714903+damuljumong@users.noreply.github.com>
2024-05-07 05:23:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -125,8 +125,10 @@ def main():
                         low_price = stock.history(period="1d")["Low"].iloc[0]  # 당일 최저가
                         high_price = stock.history(period="1d")["High"].iloc[0]  # 당일 최고가
                         st.write(f"Day's Range: {low_price:.2f} - {high_price:.2f}")
-                    except: st.write("Day's Range:)
-                    try:    st.write("52 Week Range:", stock.info["dayLow"], "-", stock.info["dayHigh"])
+                    except: st.write("Day's Range:")
+                                     
+                    try:
+                        st.write("52 Week Range:", stock.info["dayLow"], "-", stock.info["dayHigh"])                        
                     except: st.write(52 Week Range:)
 
 
